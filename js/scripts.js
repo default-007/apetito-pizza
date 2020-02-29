1a528c549e05df6a61c638263100487d5dcfca1e
Brian Otieno <brianokola@gmail.com>
2020-02-29 18:42:01 +0300

    Added a function to display and get the value of the size of pizza

--- js/scripts.js
+++ js/scripts.js
@@ -1,59 +1,13 @@
-function pizzaSize() {
-  document.getElementById("sizeDropdown").classList.toggle("show");
+function GetSizeValue() {
+  var e = document.getElementById("pizzaSize");
+  var result = e.options[e.selectedIndex].value;
+
+  document.getElementById("sprice").innerHTML = result;
 }
 
-window.onclick = function (event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
-
-function getSelected() {
-  var e = document.getElementById("sizeDropdown");
+function GetSizeText() {
+  var e = document.getElementById("pizzaSize");
   var result = e.options[e.selectedIndex].text;
-  document.getElementsByClassName("smessage").innerHTML = result;
-}
-//CRUST DROPDOWN
-function pizzaCrust() {
-  document.getElementById("crustDropdown").classList.toggle("show");
-}
 
-window.onclick = function (event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
+  document.getElementById("result").innerHTML = result;
 }
-//TOPPING DROPDOWN
-function pizzaTopping() {
-  document.getElementById("toppingDropdown").classList.toggle("show");
-}
-
-window.onclick = function (event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
-
-//
-var size = document.getElementById("sizeDropdown")
