dc05be9fe9d7cb66a2c1e17ca30f180b626eb43e
Brian Otieno <brianokola@gmail.com>
2020-03-01 16:19:42 +0300

    Addfuction to display shoping cart in table

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+//VALUE
+
 function GetSizeValue() {
   var e = document.getElementById("pizzaSize");
   var result = e.options[e.selectedIndex].value;
@@ -5,9 +7,40 @@ function GetSizeValue() {
   document.getElementById("sprice").innerHTML = result;
 }
 
+function GetCrustValue() {
+  var e = document.getElementById("pizzaCrust");
+  var result = e.options[e.selectedIndex].value;
+
+  document.getElementById("cprice").innerHTML = result;
+}
+
+function GetToppingValue() {
+  var e = document.getElementById("pizzaTopping");
+  var result = e.options[e.selectedIndex].value;
+
+  document.getElementById("tprice").innerHTML = result;
+}
+
+
+
+// TABLE TEXT
 function GetSizeText() {
   var e = document.getElementById("pizzaSize");
   var result = e.options[e.selectedIndex].text;
 
-  document.getElementById("result").innerHTML = result;
+  document.getElementById("stext").innerHTML = result;
+}
+
+function GetCrustText() {
+  var b = document.getElementById("pizzaCrust");
+  var result = b.options[b.selectedIndex].text;
+
+  document.getElementById("ctext").innerHTML = result;
+}
+
+function GetToppingText() {
+  var c = document.getElementById("pizzaTopping");
+  var result = e.options[e.selectedIndex].text;
+
+  document.getElementById("ttext").innerHTML = result;
 }
